3d547f3f64317aa675fc8645579fe4ec9e433d73
Artanias <43622365+Artanias@users.noreply.github.com>
2025-12-11 16:04:38 +0300

    Adds solution for the seventh puzzle second part.

--- puzzles/day7/parts.go
+++ puzzles/day7/parts.go
@@ -50,6 +50,54 @@ func calcRes(content string) (int64, error) {
 
 func calcRes2(content string) (int64, error) {
 	var res int64
+	field := tools.MakeField(content)
+	positions := field.FindPositions("S")
+	if len(positions) != 1 {
+		return 0, fmt.Errorf("invalid number of S positions in example file - %d", len(positions))
+	}
+	memory := make(map[tools.Point]int64, 0)
+	for len(positions) != 0 {
+		point := positions[0]
+		positions = positions[1:]
+		_, ok := memory[point]
+		if ok {
+			continue
+		}
+		if point.X-1 < 0 {
+			memory[point] = 1
+		}
+		if point.X+1 == field.Rows {
+			continue
+		}
+		if field.F[point.X+1][point.Y] == "^" {
+			positions = append(positions, tools.Point{X: point.X + 1, Y: point.Y - 1})
+			positions = append(positions, tools.Point{X: point.X + 1, Y: point.Y + 1})
+		} else {
+			positions = append(positions, tools.Point{X: point.X + 1, Y: point.Y})
+		}
+		if point.X-1 < 0 {
+			continue
+		}
+		if point.Y-1 >= 0 && field.F[point.X][point.Y-1] == "^" {
+			v, ok := memory[tools.Point{X: point.X - 1, Y: point.Y - 1}]
+			if ok {
+				memory[point] += v
+			}
+		}
+		if point.Y+1 < field.Columns && field.F[point.X][point.Y+1] == "^" {
+			v, ok := memory[tools.Point{X: point.X - 1, Y: point.Y + 1}]
+			if ok {
+				memory[point] += v
+			}
+		}
+		v, ok := memory[tools.Point{X: point.X - 1, Y: point.Y}]
+		if ok {
+			memory[point] += v
+		}
+	}
+	for j := 0; j < field.Columns; j++ {
+		res += memory[tools.Point{X: field.Rows - 2, Y: j}]
+	}
 	return res, nil
 }
 
@@ -57,6 +105,6 @@ func Run() {
 	tools.Run(
 		[]func(string) (int64, error){calcRes, calcRes2},
 		[]string{exampleFilePath, dataFilePath},
-		[]int64{21, 1649, 0, 0},
+		[]int64{21, 1649, 40, 16937871060075},
 	)
 }
