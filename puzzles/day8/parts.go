db4e536bdaadc306e5e852d596b16fe8ec704d2d
Artanias <43622365+Artanias@users.noreply.github.com>
2025-12-13 14:10:23 +0300

    Adds solution for the eight puzzle second part.

--- puzzles/day8/parts.go
+++ puzzles/day8/parts.go
@@ -14,31 +14,39 @@ var puzzlePath string = filepath.Join(puzzles.PuzzlePath, "day8")
 var exampleFilePath string = filepath.Join(puzzlePath, "example.txt")
 var dataFilePath string = filepath.Join(puzzlePath, "data.txt")
 
-func findMinDistPoints(n int, points []tools.Point3D) []tools.PointsPair {
+func findMinDistPoints(condition bool, points []tools.Point3D) chan tools.PointsPair {
 	pointsPairs := make([]tools.PointsPair, 0)
-	for i := 0; i < n; i++ {
-		minDist := 0
-		var minPoint1, minPoint2 tools.Point3D
-		for _, p1 := range points {
-			for _, p2 := range points {
-				if p1 == p2 {
-					continue
-				}
-				dist := p1.Distance(p2)
-				if (minDist == 0 || minDist > dist) && !slices.Contains(
-					pointsPairs, tools.PointsPair{P1: p1, P2: p2},
-				) && !slices.Contains(
-					pointsPairs, tools.PointsPair{P1: p2, P2: p1},
-				) {
-					minDist = dist
-					minPoint1 = p1
-					minPoint2 = p2
+	c := make(chan tools.PointsPair, 1)
+
+	go func() {
+		for condition {
+			minDist := 0
+			var minPoint1, minPoint2 tools.Point3D
+			for _, p1 := range points {
+				for _, p2 := range points {
+					if p1 == p2 {
+						continue
+					}
+					dist := p1.Distance(p2)
+					if (minDist == 0 || minDist > dist) && !slices.Contains(
+						pointsPairs, tools.PointsPair{P1: p1, P2: p2},
+					) && !slices.Contains(
+						pointsPairs, tools.PointsPair{P1: p2, P2: p1},
+					) {
+						minDist = dist
+						minPoint1 = p1
+						minPoint2 = p2
+					}
 				}
 			}
+			pointsPair := tools.PointsPair{P1: minPoint1, P2: minPoint2}
+			c <- pointsPair
+			pointsPairs = append(pointsPairs, pointsPair)
 		}
-		pointsPairs = append(pointsPairs, tools.PointsPair{P1: minPoint1, P2: minPoint2})
-	}
-	return pointsPairs
+		close(c)
+	}()
+
+	return c
 }
 
 func deepContains(point tools.Point3D, circuits [][]tools.Point3D) int {
@@ -73,10 +81,16 @@ func calcRes(content string, minPointsCnt int) (int64, error) {
 		points = append(points, tools.Point3D{X: x, Y: y, Z: z})
 	}
 	circuits := make([][]tools.Point3D, 0)
-	minDistPoints := findMinDistPoints(minPointsCnt, points)
-	for _, pointsPair := range minDistPoints {
+	condition := true
+	k := 0
+	for pointsPair := range findMinDistPoints(condition, points) {
+		if k == minPointsCnt {
+			condition = false
+			break
+		}
 		i := deepContains(pointsPair.P1, circuits)
 		j := deepContains(pointsPair.P2, circuits)
+		k++
 		if i != -1 && j != -1 && i == j {
 			continue
 		} else if i != -1 && j != -1 {
@@ -104,6 +118,51 @@ func calcRes(content string, minPointsCnt int) (int64, error) {
 
 func calcRes2(content string) (int64, error) {
 	var res int64
+	points := make([]tools.Point3D, 0)
+	for line := range strings.SplitSeq(content, "\n") {
+		numbers := strings.Split(line, ",")
+		if len(numbers) != 3 {
+			return 0, fmt.Errorf("invalid line: %s", line)
+		}
+		x, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			return 0, err
+		}
+		y, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			return 0, err
+		}
+		z, err := strconv.Atoi(numbers[2])
+		if err != nil {
+			return 0, err
+		}
+		points = append(points, tools.Point3D{X: x, Y: y, Z: z})
+	}
+	circuits := make([][]tools.Point3D, 0)
+	condition := true
+	for pointsPair := range findMinDistPoints(condition, points) {
+		i := deepContains(pointsPair.P1, circuits)
+		j := deepContains(pointsPair.P2, circuits)
+		if i != -1 && j != -1 && i == j {
+			continue
+		} else if i != -1 && j != -1 {
+			circuits[i] = append(circuits[i], circuits[j]...)
+			circuits = slices.Delete(circuits, j, j+1)
+		} else if i != -1 {
+			circuits[i] = append(circuits[i], pointsPair.P2)
+		} else if j != -1 {
+			circuits[j] = append(circuits[j], pointsPair.P1)
+		} else {
+			circuit := make([]tools.Point3D, 0)
+			circuit = append(circuit, pointsPair.P1, pointsPair.P2)
+			circuits = append(circuits, circuit)
+		}
+		if len(circuits) == 1 && len(circuits[0]) == len(points) {
+			res = int64(pointsPair.P1.X) * int64(pointsPair.P2.X)
+			condition = false
+			break
+		}
+	}
 	return res, nil
 }
 
@@ -121,6 +180,6 @@ func Run() {
 	tools.Run(
 		[]func(string) (int64, error){runner(), calcRes2},
 		[]string{exampleFilePath, dataFilePath},
-		[]int64{40, 112230, 0, 0},
+		[]int64{40, 112230, 25272, 2573952864},
 	)
 }
